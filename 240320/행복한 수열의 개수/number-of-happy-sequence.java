9bb8d25a9d8f0aa556b08e4204b771fc8980adbb
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-03-20 22:55:02 +0900

    Title: 행복한 수열의 개수, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240320/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[][] arr;
+    static int ans;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][N];
+
+        for(int r=0; r<N; r++){
+            st = new StringTokenizer(br.readLine());
+            for(int c=0; c<N; c++){
+                arr[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int r=0; r<N; r++){
+            if(isHappyRow(r)) ans++;
+        }
+
+        for(int c=0; c<N; c++){
+            if(isHappyCol(c)) ans++;
+        }
+
+        sb.append(ans);
+
+        System.out.print(sb);
+    }
+
+    static boolean isHappyRow(int row){
+        int before = arr[row][0];
+        int cnt = 1;
+        for(int c=1; c<N; c++){
+            if(arr[row][c] == before){
+                cnt++;
+                if(cnt == M) return true;
+            }else{
+                before = arr[row][c];
+                cnt = 1;
+            }
+        }
+        return false;
+    }
+
+    static boolean isHappyCol(int col){
+        int before = arr[0][col];
+        int cnt = 1;
+        for(int r=1; r<N; r++){
+            if(arr[r][col] == before){
+                cnt++;
+                if(cnt == M) return true;
+            }else{
+                before = arr[r][col];
+                cnt = 1;
+            }
+        }
+        return false;
+    }
+}
