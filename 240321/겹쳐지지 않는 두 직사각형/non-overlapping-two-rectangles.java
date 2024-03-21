1b1c2052611fd034f9673502896d44f4ab56aa89
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-03-21 23:50:05 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 148ms, Memory: 9MB, Status: Passed - CodeTree

--- 240321/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
+++ 240321/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.java
@@ -8,14 +8,14 @@ public class Main {
 
     static int N, M;
     static int[][] arr;
-    static int sr, sc, er, ec;
-    static int max1, max2;
+    static int max;
 
     public static void main(String[] args) throws IOException{
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         arr = new int[N][M];
+        max = Integer.MIN_VALUE;
 
         for(int n=0; n<N; n++){
             st = new StringTokenizer(br.readLine());
@@ -24,50 +24,43 @@ public class Main {
             }
         }
 
-
-        max1 = max2 = Integer.MIN_VALUE;
-        for(int r=0; r<N; r++){
-            for(int c=0; c<M; c++){
-                findMax1(r, c);
+        for(int sr1=0; sr1<N; sr1++){
+            for(int sc1=0; sc1<M; sc1++){
+                for(int er1=sr1; er1<N; er1++){
+                    for(int ec1=sc1; ec1<M; ec1++){
+                        for(int sr2=0; sr2<N; sr2++){
+                            for(int sc2=0; sc2<M; sc2++){
+                                for(int er2=sr2; er2<N; er2++){
+                                    for(int ec2=sc2; ec2<M; ec2++){
+                                        if(check(sr1, sc1, er1, ec1, sr2, sc2, er2, ec2)) continue;
+                                        int sum1 = getAreaSum(sr1, sc1, er1, ec1);
+                                        int sum2 = getAreaSum(sr2, sc2, er2, ec2);
+                                        max = Math.max(max, sum1 + sum2);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
 
-        for(int r=0; r<N; r++){
-            for(int c=0; c<M; c++){
-                if(pass(r, c)) continue;
-                findMax2(r, c);
-            }
-        }
-
-        sb.append(max1 + max2);
+        sb.append(max);
 
         System.out.print(sb);
     }
 
-    static void findMax1(int row, int col){
-        for(int r=row; r<N; r++){
-            for(int c=col; c<M; c++){
-                int sum = getSum(row, col, r, c);
-                if(sum > max1){
-                    max1 = sum;
-                    sr = row;
-                    sc = col;
-                    er = r;
-                    ec = c;
-                }
+    static boolean check(int sr1, int sc1, int er1, int ec1, int sr2, int sc2, int er2, int ec2){
+        for(int r=sr1; r<=er1; r++){
+            for(int c=sc1; c<=ec1; c++){
+                if(r>=sr2 && r<=er2 && c>=sc2 && c<=ec2) return true;
             }
         }
+
+        return false;
     }
 
-    static void findMax2(int row, int col){
-        for(int r=row; r<N; r++){
-            for(int c=col; c<M; c++){
-                max2 = Math.max(max2, getSum(row, col, r, c));
-            }
-        }
-    }
-
-    static int getSum(int sr, int sc, int er, int ec){
+    static int getAreaSum(int sr, int sc, int er, int ec){
         int sum = 0;
         for(int r=sr; r<=er; r++){
             for(int c=sc; c<=ec; c++){
@@ -76,9 +69,5 @@ public class Main {
         }
 
         return sum;
-    }
-
-    static boolean pass(int r, int c){
-        return r>=sr && r<=er && c>=sc && c<=ec;
     }
 }
