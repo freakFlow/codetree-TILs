cd60fe0f5e678807663a06c1a4dd61a9927d523c
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-03-25 20:31:07 +0900

    Title: 1차원 폭발 게임, Time: 98ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240325/1차원 폭발 게임/The-1D-bomb-game.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] arr;
+    static int[] temp;
+    static int endIdx;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        temp = new int[N];
+        endIdx = N;
+
+        for(int n=0; n<N; n++){
+            arr[n] = Integer.parseInt(br.readLine());
+        }
+
+        while(true){
+            int num = arr[0];
+            int start = 0;
+            int end = 0;
+            for(int n=1; n<endIdx; n++){
+                if(arr[n] == num){
+                    end++;
+                }else{
+                    if(end - start + 1 >= M) break;
+                    num = arr[n];
+                    start = end = n;
+                }
+            }
+
+            if(end - start + 1 < M) break;
+
+            for(int n=start; n<=end; n++){
+                arr[n] = -1;
+            }
+
+            int tIdx = 0;
+            for(int n=0; n<endIdx; n++){
+                if(arr[n] == -1) continue;
+                temp[tIdx++] = arr[n];
+            }
+
+            endIdx = tIdx;
+            for(int n=0; n<endIdx; n++){
+                arr[n] = temp[n];
+            }
+        }
+
+        sb.append(endIdx).append('\n');
+        for(int n=0; n<endIdx; n++){
+            sb.append(arr[n]).append('\n');
+        }
+
+        System.out.print(sb);
+    }
+}
