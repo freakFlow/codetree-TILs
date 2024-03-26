f1b2593432c18368473790a6b78ff8355f50981c
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-03-26 23:41:40 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 3045ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240326/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] dr = {0, 1};
+    static int[] dc = {1, 0};
+    static int N, M;
+    static boolean[][] visited;
+    static int ans;
+
+    public static void main(String[] args) throws IOException{
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N][M];
+
+        for(int n=0; n<N; n++){
+            st = new StringTokenizer(br.readLine());
+            for(int m=0; m<M; m++){
+                if(st.nextToken().equals("1")) visited[n][m] = true;
+            }
+        }
+
+        visited[0][0] = true;
+        dfs(0, 0);
+
+        sb.append(ans);
+
+        System.out.print(sb);
+    }
+
+    static void dfs(int r, int c){
+        if(isEnd(r, c)){
+            ans = 1;
+            return;
+        }
+        
+        for(int d=0; d<dr.length; d++){
+            int nr = r + dr[d];
+            int nc = c + dc[d];
+            if(isIn(nr, nc) && visited[nr][nc]){
+                visited[nr][nc] = true;
+                dfs(nr, nc);
+            }
+        }
+    }
+
+    static boolean isIn(int r, int c){
+        return r>=0 && r<N && c>=0 && c<M;
+    }
+
+    static boolean isEnd(int r, int c){
+        return r == N-1 && c == M-1;
+    }
+}
