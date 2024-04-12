5d15f020be32ae5ce5146e84d139ba28be68a8f0
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-04-12 15:35:09 +0900

    Title: 세 수의 합, Time: 363ms, Memory: 44MB, Status: Passed - CodeTree

--- 240412/세 수의 합/sum-of-three-num.java
+++ 240412/세 수의 합/sum-of-three-num.java
@@ -9,7 +9,7 @@ public class Main {
     static int N, K;
     static int[] arr;
     static HashMap<Integer, Integer> hashMap;
-    static int n1, n2, n3, c1, c2, c3;
+    static int n1, n2, n3;
     static int ans;
 
     public static void main(String[] args) throws IOException{
@@ -27,38 +27,34 @@ public class Main {
         }
 
         for(int i=0; i<N; i++){
-            n1 = arr[i];
-            c1 = hashMap.getOrDefault(n1, 0);
-            if(c1 == 0) continue;
-            for(int j=0; j<N; j++){
+            for(int j=i+1; j<N; j++){
+                n1 = arr[i];
                 n2 = arr[j];
-                c2 = hashMap.getOrDefault(n2, 0);
-                if(c2 == 0) continue;
-
                 n3 = K - (n1 + n2);
-                c3 = hashMap.getOrDefault(n3, 0);
-                if(c3 == 0) continue;
 
-                if(n1 == n2 && n2 == n3){
-                    ans += c1 * (c1-1) * (c1-2) / 6;
-                }else if(n1 == n2){
-                    ans += c1 * (c1-1) / 2 * c3;
-                }else if(n2 == n3){
-                    ans += c2 * (c2-1) / 2 * c1;
-                }else if(n1 == n3){
-                    ans += c3 * (c3-1) / 2 * c2;
-                }else{
-                    ans += c1 * c2 * c3;
-                }
-
-                hashMap.remove(n1);
-                hashMap.remove(n2);
-                hashMap.remove(n3);
+                minus(n1);
+                minus(n2);
+                int count = hashMap.getOrDefault(n3, 0);
+                if(count > 0) ans += count;
+                plus(n1);
+                plus(n2);
             }
         }
 
-        sb.append(ans);
+        sb.append(ans / 3);
 
         System.out.print(sb);
+    }
+
+    static void minus(int key){
+        if(!hashMap.containsKey(key)) return;
+        int value = hashMap.get(key);
+        hashMap.put(key, value - 1);
+    }
+
+    static void plus(int key){
+        if(!hashMap.containsKey(key)) return;
+        int value = hashMap.get(key);
+        hashMap.put(key, value + 1);
     }
 }
