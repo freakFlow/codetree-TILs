72377dcf7ab4dd00dd6b91e80d4e06c64ee223fe
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-05-24 14:32:03 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 117ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240524/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    static int N, M, T;
+    static int[][] arr;
+    static boolean[][] marble;
+    static int[][] count;
+    static int ans;
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        T = Integer.parseInt(st.nextToken());
+        arr = new int[N][N];
+        marble = new boolean[N][N];
+        count = new int[N][N];
+
+        for(int r=0; r<N; r++){
+            st = new StringTokenizer(br.readLine());
+            for(int c=0; c<N; c++){
+                arr[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int m=0; m<M; m++){
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken()) - 1;
+            marble[r][c] = true;
+        }
+
+        for(int t=0; t<T; t++){
+            for(int r=0; r<N; r++){
+                for(int c=0; c<N; c++){
+                    if(!marble[r][c]) continue;
+
+                    marble[r][c] = false;
+                    int max = 0, mr = 0, mc = 0;
+                    for(int d=0; d<dr.length; d++){
+                        int nr = r + dr[d];
+                        int nc = c + dc[d];
+
+                        if(isIn(nr, nc) && arr[nr][nc] > max){
+                            max = arr[nr][nc];
+                            mr = nr;
+                            mc = nc;
+                        }
+                    }
+
+                    count[mr][mc]++;
+                }
+            }
+
+            for(int r=0; r<N; r++){
+                for(int c=0; c<N; c++){
+                    if(count[r][c] == 1) marble[r][c] = true;
+                    count[r][c] = 0;
+                }
+            }
+        }
+
+        for(int r=0; r<N; r++){
+            for(int c=0; c<N; c++){
+                if(marble[r][c]) ans++;
+            }
+        }
+
+        sb.append(ans);
+
+        System.out.print(sb);
+    }
+
+    static boolean isIn(int r, int c){
+        return r>=0 && r<N && c>=0 && c<N;
+    }
+}
