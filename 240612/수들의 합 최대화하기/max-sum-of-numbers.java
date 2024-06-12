c9aadbb65fbf87bc754ff1b813dd77f623cd7f6b
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-06-12 11:24:37 +0900

    Title: 수들의 합 최대화하기, Time: 1257ms, Memory: 9MB, Status: Passed - CodeTree

--- 240612/수들의 합 최대화하기/max-sum-of-numbers.java
+++ 240612/수들의 합 최대화하기/max-sum-of-numbers.java
@@ -7,47 +7,44 @@ public class Main {
     static StringTokenizer st;
 
     static int N;
-    static int[][] arr;
+    static int[] arr;
     static int ans;
 
     static boolean[] row, col;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
+        arr = new int[N * N];
         row = new boolean[N];
         col = new boolean[N];
 
         for(int r=0; r<N; r++){
             st = new StringTokenizer(br.readLine());
             for(int c=0; c<N; c++){
-                arr[r][c] = Integer.parseInt(st.nextToken());
+                arr[r * N + c] = Integer.parseInt(st.nextToken());
             }
         }
 
-        findMax(0, 0);
+        findMax(0, 0, 0);
 
         sb.append(ans);
 
         System.out.print(sb);
     }
 
-    static void findMax(int idx, int sum){
+    static void findMax(int idx, int start, int sum){
         if(idx == N){
             ans = Math.max(ans, sum);
             return;
         }
 
-        for(int r=0; r<N; r++){
-            if(row[r]) continue;
-            row[r] = true;
-            for(int c=0; c<N; c++){
-                if(col[c]) continue;
-                col[c] = true;
-                findMax(idx + 1, sum + arr[r][c]);
-                col[c] = false;
-            }
-            row[r] = false;
+        for(int i=start; i<N * N; i++){
+            if(row[i / N] || col[i % N]) continue;
+            row[i / N] = true;
+            col[i % N] = true;
+            findMax(idx + 1, i + 1, sum + arr[i]);
+            row[i / N] = false;
+            col[i % N] = false;
         }
     }
 }
