984da7f5200b774d38dbcc099c24bc4883d507b3
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-06-12 11:44:33 +0900

    Title: 수들 중 최솟값 최대화하기, Time: 3010ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 240612/수들 중 최솟값 최대화하기/maximin-of-numbers.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    static final int INF = 10_001;
+
+    static int N;
+    static int[] arr;
+    static int ans;
+
+    static boolean[] row, col;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N * N];
+
+        for(int r=0; r<N; r++){
+            st = new StringTokenizer(br.readLine());
+            for(int c=0; c<N; c++){
+                arr[r * N + c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        row = new boolean[N];
+        col = new boolean[N];
+
+        findIt(0, INF);
+
+        sb.append(ans);
+
+        System.out.print(sb);
+    }
+
+    static void findIt(int idx, int min){
+        if(idx == N){
+            ans = Math.max(ans, min);
+            return;
+        }
+
+        for(int i=0; i<N*N; i++){
+            if(row[i / N] || col[i % N]) continue;
+            row[i / N] = true;
+            col[i % N] = true;
+            findIt(idx + 1, Math.min(min, arr[i]));
+            row[i / N] = false;
+            col[i % N] = false;
+        }
+    }
+}
