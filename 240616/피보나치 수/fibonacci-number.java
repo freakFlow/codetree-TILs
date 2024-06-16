54c247b7f7f7be163e7c2f5269fe23daa6429182
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-06-16 10:03:21 +0900

    Title: 피보나치 수, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 240616/피보나치 수/fibonacci-number.java
+++ 240616/피보나치 수/fibonacci-number.java
@@ -6,16 +6,18 @@ public class Main {
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
 
+    static final int MAX = 45;
+
     static int N;
     static int[] dp;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        dp = new int[N + 1];
+        dp = new int[MAX + 1];
 
         dp[1] = dp[2] = 1;
 
-        for(int i=3; i<=N; i++){
+        for(int i=3; i<=MAX; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
 
