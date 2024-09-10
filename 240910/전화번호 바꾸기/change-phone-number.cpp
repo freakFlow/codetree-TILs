dca676ba2bf32539658d2a4ff174016111ca4609
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-09-10 12:45:54 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240910/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin.get();
+    cin.get();
+    cin.get();
+    cin.get();
+    cin >> a;
+    cin.get();
+    cin >> b;
+
+    cout << "010-" << b << "-" << a;
+
+    return 0;
+}
