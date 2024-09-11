fbcbeb883383abbaa0f571eea748f5fcae71cf7e
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-09-11 10:08:07 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240911/체질량지수/bmi.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int h, w;
+    int b;
+
+    cin >> h >> w;
+
+    b = 10000 * w / (h * h);
+
+    cout << b;
+    if(b >= 25) cout << "\nObesity";
+
+    return 0;
+}
