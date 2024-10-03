9ae33de95e974639e96b410cbe318d83ce6032cb
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-03 22:23:20 +0900

    Title: 1까지 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/1까지 나누기/divide-by-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for(int i=1; ; i++){
+        n /= i;
+        if(n <= 1){
+            cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
