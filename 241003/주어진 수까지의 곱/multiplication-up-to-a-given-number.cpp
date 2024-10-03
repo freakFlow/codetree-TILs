cc1a45de5b77285b92806f259841d64b7bf1c8fb
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-03 21:26:25 +0900

    Title: 주어진 수까지의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/주어진 수까지의 곱/multiplication-up-to-a-given-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    int mul = 1;
+
+    cin >> a >> b;
+
+    for(int i=a; i<=b; i++){
+        mul *= i;
+    }
+
+    cout << mul;
+
+    return 0;
+}
