67e3fe25905c8913ef0f78b056038e0bd704bb0d
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-05 16:49:33 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241005/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    int cnt = 1;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << cnt % 8 + 1 << ' ';
+            cnt += 2;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
