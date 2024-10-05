ba4a4a36598ff8a55630a1382b8f49f9c9a322b0
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-05 10:49:58 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241005/체크 다이아몬드/check-diamond.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    int cnt = 1;
+
+    for(int i=0; i<2*n-1; i++){
+        for(int j=0; j<n-cnt; j++){
+            cout << ' ';
+        }
+        for(int j=0; j<cnt; j++){
+            cout << "* ";
+        }
+        cout << endl;
+
+        if(i >= n-1) cnt--;
+        else cnt++;
+    }
+
+    return 0;
+}
