84916647bd0995cd2d0c77997133ce6d4df7c72d
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-06 17:40:36 +0900

    Title: ab곱 n번 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/ab곱 n번 구하기/a-multiple-b-n-times.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, a, b;
+
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        cin >> a >> b;
+
+        int mul = 1;
+        for(int j=a; j<=b; j++){
+            mul *= j;
+        }
+        cout << mul << endl;
+    }
+    
+    return 0;
+}
