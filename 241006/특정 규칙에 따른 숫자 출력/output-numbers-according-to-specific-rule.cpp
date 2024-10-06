e03bd382e189ee2d43a072cdf464268aff509579
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-06 14:34:43 +0900

    Title: 특정 규칙에 따른 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/특정 규칙에 따른 숫자 출력/output-numbers-according-to-specific-rule.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    int cnt = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(j < i) cout << "  ";
+            else cout << (cnt++) % 9 + 1 << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
