ab9ab3dffd14b6c2c38ec55f0bd58ba82d0ba529
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-06 14:23:43 +0900

    Title: 구구단 만들기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/구구단 만들기 2/print-multiplication-table-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+
+    cin >> a >> b;
+
+    for(int i=1; i<=4; i++){
+        for(int j=b; j>=a; j--){
+            cout << j << " * " << i * 2 << " = " << j * (i * 2);
+            if(j > a) cout << " / ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
