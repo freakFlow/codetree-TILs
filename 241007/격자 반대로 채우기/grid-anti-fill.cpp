0b10972596baa545a983b0d17600c17c4f9c2397
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-07 23:22:47 +0900

    Title: 격자 반대로 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/격자 반대로 채우기/grid-anti-fill.cpp
+++ 241007/격자 반대로 채우기/grid-anti-fill.cpp
@@ -8,15 +8,18 @@ int main() {
     cin >> n;
 
     int num = 1;
+    bool isUp = true;
     for(int c=n-1; c>=0; c--){
-        if(c % 2 == 0){
+        if(!isUp){
             for(int r=0; r<n; r++){
                 arr[r][c] = num++;
             }
+            isUp = !isUp;
         }else{
             for(int r=n-1; r>=0; r--){
                 arr[r][c] = num++;
             }
+            isUp = !isUp;
         }
     }
 
