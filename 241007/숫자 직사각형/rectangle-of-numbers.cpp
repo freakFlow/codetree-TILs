4185d920b3c66da91fbedc28935537e11fb2b7b3
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-07 23:01:49 +0900

    Title: 숫자 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241007/숫자 직사각형/rectangle-of-numbers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    
+    cin >> n >> m;
+
+    int num = 1;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout << num++ << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
