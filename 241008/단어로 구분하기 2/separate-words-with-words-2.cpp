481ad3a2a0b1c0fe377b441f6baa3a2db083a5cf
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-08 22:34:51 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241008/단어로 구분하기 2/separate-words-with-words-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+
+    for(int i=0; i<10; i++){
+        cin >> str;
+        if(i % 2 == 0) cout << str << endl;
+    }
+
+    return 0;
+}
