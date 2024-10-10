e9455593795264985fe9571c197a45634d2e108a
freakFlow <114668941+freakFlow@users.noreply.github.com>
2024-10-10 22:43:52 +0900

    Title: 두 번째를 첫 번째로, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/두 번째를 첫 번째로/second-to-first.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    char ch1, ch2;
+
+    cin >> str;
+    ch1 = str[0];
+    ch2 = str[1];
+
+    for(int i=0; i<str.length(); i++){
+        if(str[i] == ch2) cout << ch1;
+        else cout << str[i];
+    }
+
+    return 0;
+}
